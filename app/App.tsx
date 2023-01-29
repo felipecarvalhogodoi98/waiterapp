eeb6c01d0ec7f507295ef8ac714e71ad9b2f7872
felipec.godoi98 <felipec.godoi98@gmail.com>
2023-01-29 11:37:05 -0300

    feat(app): inital configs

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Text } from "./src/components/Text";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+  });
+
+  if (!isFontsLoaded) return null;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
